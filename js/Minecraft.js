1a3ae6269b63f214a8971e7beee5ce5cf78087ed
Jordan Galler <jordan.galler@gmail.com>
2017-01-03 23:08:24 +0200

    Added randomization to the terrain generation; put terrain generator in a seperate function.

--- js/Minecraft.js
+++ js/Minecraft.js
@@ -54,21 +54,33 @@ var Minecraft = {
     },
 
     makeStartMatrix: function () {
+                Minecraft.makeStartTerrain();
+    },
+
+    makeStartTerrain: function(){
         for (var i = Minecraft.matrix.length - 1; i >= 2 * (x / 3); i--) {
             for (var j = 0; j < Minecraft.matrix[i].length; j++) {
-                if (i === 2 * (x / 3)) {
-                    Minecraft.matrix[i][j] = "grass";
-                }
-                else {
+                if (i === Minecraft.matrix.length - 1) { //bottom row is all dirt...(We should write it so that 1/5 of the bottom 1/3 is always dirt...).
                     Minecraft.matrix[i][j] = "dirt";
+
+                } else { //****This is how we randomize the terrain****
+                    if (Minecraft.matrix[i + 1][j] == "dirt") { //if the cell below the current cell has dirt...
+
+                        var rand = Math.round(Math.random()); //generate a random num between 0 and 1...
+
+                        if (rand > 0 && i > 2 * (x / 3)) {     //and if the number is 1 and the row is not 1/3 of the matrix length...
+
+                            Minecraft.matrix[i][j] = "dirt"; //put dirt in the current cell.
+
+                        } else {
+                            Minecraft.matrix[i][j] = "grass"; //if the number is 0, make the current cell grass.
+                        }//The bug: if x is not divisible by 5, we still get dirt on the topmost row. It should always be grass.
+
+                    }
                 }
+
             }
         }
-        for (var i = 2 * (x/3) + 1; i > x/3; i--){
-            
-        }
-
-
     },
 
     renderStartMatrix: function () {
@@ -81,11 +93,10 @@ var Minecraft = {
     }
 
 
-
 };
 
 
 var x = 15;
-var y = 25;
+var y = 20;
 
 Minecraft.init(x, y);
