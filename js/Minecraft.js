2b1174eea3ea386a5a8f1cef6b6863edd4778288
Jordan Galler <jordan.galler@gmail.com>
2017-01-03 14:58:03 +0200

    Started js, added images.

--- js/Minecraft.js
+++ js/Minecraft.js
@@ -1,1 +1,59 @@
-$("<p/>").text("Hello!").appendTo("body")
+var Minecraft = {
+
+    init: function (x, y) {
+        Minecraft.drawGrid(x, y);
+        Minecraft.buildMatrix(x, y);
+
+    },
+
+    drawGrid: function (x, y) {
+        // we create our grid 
+        for (var i = 0; i < x; i++) {
+            var row = $("<div/>");
+            row.addClass("row");
+            $("#grid").append(row);
+            for (var j = 0; j < y; j++) {
+                var cell = $("<div/>");
+                cell.addClass("cell");
+                row.append(cell);
+            }
+        }
+    },
+
+    buildMatrix: function (x, y) {
+        //we create our matrix 
+        Minecraft.matrix = new Array(x);
+        Minecraft.cells = $(".cell");
+        for (var i = 0; i < Minecraft.matrix.length; i++) {
+            Minecraft.matrix[i] = new Array(y);
+        }
+
+        //We link our matrix to our grid
+        for (var i = 0; i < Minecraft.matrix.length; i++) {
+            for (var j = 0; j < Minecraft.matrix[i].length; j++) {
+                Minecraft.cells.eq(i * y + j)
+                    .data("i", i)
+                    .data("j", j);
+            }
+        }
+
+    },
+
+    renderGrid: function () {
+
+    },
+
+    pickTool: function () {
+    },
+
+    changeCell: function () {
+    }
+
+
+};
+
+
+var x = 15;
+var y = 25;
+
+Minecraft.init(x, y);
